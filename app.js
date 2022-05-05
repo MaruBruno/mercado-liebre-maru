5365ae5974572418754772336803bae55ddd06bc
MaruBruno <marubruno@gmail.com>
2022-05-05 18:13:44 -0300

    Intentar Heroku

---
+++ app.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const app = express();
+const path = require("path");
+
+//console.log(express);
+
+const port = process.env.PORT || 3000;
+
+app.use(express.static(path.resolve(__dirname, 'public')));
+
+app.listen(port, () => {
+  console.log('Servidor Funcionando');
+});
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "./views/home.html"))
+})
+
+app.get('/views/register.html', (req, res) => {
+  res.sendFile(path.join(__dirname, "./views/register.html"))
+})
+
+app.get('/views/login.html', (req, res) => {
+  res.sendFile(path.join(__dirname, "./views/login.html"))
+})
